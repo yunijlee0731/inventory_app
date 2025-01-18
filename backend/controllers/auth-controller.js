117e5e49424e278678a484d80ff4033ad04fd180
YJ Lee <yunijlee0731@gmail.com>
2025-01-17 18:18:37 -0800

    working sign up page, im still not sure if the error from the preivous push still persists

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,5 +1,4 @@
 // TODO: HASH THE PASSWORD
-
 const db = require("../config/db-config");
 
 exports.checkLogin = async (req, res) => {
@@ -51,27 +50,7 @@ exports.checkLogin = async (req, res) => {
 };
 
 exports.checkUserCreation = async (req, res) => {
-  // try {
-  //     // Hash the plain text password
-  //     const hashedPassword = await bcrypt.hash(plainTextPassword, 10);
-
-  //     // Store the hashed password in the database
-  //     const query = `
-  //         INSERT INTO users (first_name, last_name, username, password)
-  //         VALUES (?, ?, ?, ?)
-  //     `;
-  //     const values = [firstName, lastName, username, hashedPassword];
-
-  //     const [result] = await db.execute(query, values);
-
-  //     console.log('User registered with ID:', result.insertId);
-  // } catch (error) {
-  //     console.error('Error registering user:', error);
-  // }
-
   try {
-    // console.log("HERE IS THE REQUEST");
-    // console.log(req);
     const [data] = await db.query("SELECT * FROM users WHERE username = ?", [
       req.body.username,
     ]); // using await to wait for the database query to complete
@@ -88,15 +67,11 @@ exports.checkUserCreation = async (req, res) => {
       ];
       const [result] = await db.execute(query, values);
       console.log("User registered with ID:", result.insertId);
-      return res.status(200).sned({
+      return res.status(200).send({
         success: true,
         message: "Account created succesfully!",
+        data,
       });
-      //   return res.status(200).send({
-      //     success: true,
-      //     message: "Account created succesfully",
-      //     data,
-      //   });
     } else {
       return res.status(404).send({
         // If no data, send 404 response
@@ -105,15 +80,20 @@ exports.checkUserCreation = async (req, res) => {
       });
     }
   } catch (error) {
-    console.log(
-      "\t*************** YJ: ERROR in auth-controller.js in checkUserCreation function: PRINTING STACK TRACE*************"
-    );
     console.error("Error:", error);
+    // TODO: Delete these error messages, they are for development purposes
     if (!res.headersSent) {
       // Check if headers are already sent
       return res.status(500).send({
         success: false,
-        message: "ERROR in auth-controller.js, in checkUserCreation function",
+        message:
+          "Backend error: in auth-controller.js, header has already been sent in checkUserCreation function",
+        error: error.message,
+      });
+    } else {
+      return res.status(500).send({
+        success: false,
+        message: "Backend error: unable to add to database",
         error: error.message,
       });
     }
